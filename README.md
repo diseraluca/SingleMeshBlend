e0d89e0092e86f10a8d8a4e4c37a545132cb4331
Luca Di sera <39652885+diseraluca@users.noreply.github.com>
2018-07-11 22:40:03 +0200

    V1.0 (#1)
    * Create LICENSE
    
    * Create .gitignore
    
    * - Initial Commit
    
    * - Wrote the base structure code for the deformer
    
    * - Structured pluginMain for the node registration
    
    * - Wrote the deformer initialize method
    
    * - Written the deform method.
    
    * - Remade the README file
    
    * Update README.md
    
    - Corrected the heading markdown
    
    * Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # SingleMeshBlend
-A didactical project to get accustomated with plugin's optimization.
+
+The *SingleMeshBlend* plugin registers a deformer node with maya.
+The registered deformer name is **SingleBlendMesh**.
+
+The **SingleBlendMesh** deformer transform a mesh into another.
+```diff
+-WARNING: The meshes should have the same number of vertices and the same vertices ID.
+```
+
+### Usage
+
+To use the **SingleBlendMesh** deformer you have to apply it to a mesh with the following **MEL** command:
+```
+deformer -type SingleBlendMesh;
+```
+This will apply the deformer to the selected mesh. If you want to apply it to a specific mesh you can use the following command:
+```
+deformer -type SingleBlendMesh MeshName;
+```
+where *MeshName* is the name of the mesh the deformer will be applied to.
+
+After the deformer is applied you will see a warning in the script editor. Nothing to worry about. It's the deformer telling us that no mesh is connected to its *blendMesh* attribute.
+To make the warning disappear and the deformer work you have to connect the *outMesh* attribute of the **mesh shape** you want to blend to to the *blendMesh* attribute of the deformer.
+
+The deformer is now working. You can decide how much to blend as a whole by using the deformer's *blendWeight* keyable attribute or you can decide the blend value per vertex by painting the weights of the deformer trough **Modify->Paint Attributes** tool in the maya UI.
+
+
+### LICENSE
+All the code in this repository is under the **MIT License**.
+More informations can be found in the *LICENSE* file in the root directory of this repository.
+
+**Copyright 2018 Luca Di Sera**
+
+*Contacts:*
+*     disera.luca@gmail.com
+*     https://github.com/diseraluca
+*     www.linkedin.com/in/luca-di-sera-200023167
---
+++ SingleMeshBlend/SingleMeshBlend/SingleBlendMeshDeformer.cpp
@@ -0,0 +1,85 @@
+// Copyright 2018 Luca Di Sera
+//		Contact: disera.luca@gmail.com
+//				 https://github.com/diseraluca
+//				 https://www.linkedin.com/in/luca-di-sera-200023167
+//
+// This code is licensed under the MIT License. 
+// More informations can be found in the LICENSE file in the root folder of this repository
+//
+//
+// File : SingleBlendMeshDeformer.cpp
+
+#include "SingleBlendMeshDeformer.h"
+
+#include <maya/MFnTypedAttribute.h>
+#include <maya/MFnNumericAttribute.h>
+#include <maya/MGlobal.h>
+#include <maya/MFnMesh.h>
+#include <maya/MItGeometry.h>
+
+MString SingleBlendMeshDeformer::typeName{ "SingleBlendMesh" };
+MTypeId SingleBlendMeshDeformer::typeId{ 0x0d12309 };
+
+MObject SingleBlendMeshDeformer::blendMesh;
+MObject SingleBlendMeshDeformer::blendWeight;
+
+void * SingleBlendMeshDeformer::creator()
+{
+	return new SingleBlendMeshDeformer();
+}
+
+MStatus SingleBlendMeshDeformer::initialize()
+{
+	MStatus status{};
+
+	MFnTypedAttribute   tAttr{};
+	MFnNumericAttribute nAttr{};
+
+	blendMesh = tAttr.create("blendMesh", "blm", MFnData::kMesh, &status);
+	CHECK_MSTATUS_AND_RETURN_IT(status);
+	CHECK_MSTATUS(addAttribute(blendMesh));
+
+	blendWeight = nAttr.create("blendWeight", "blw", MFnNumericData::kDouble, 0.0, &status);
+	CHECK_MSTATUS_AND_RETURN_IT(status);
+	CHECK_MSTATUS(nAttr.setKeyable(true));
+	CHECK_MSTATUS(nAttr.setMin(0.0));
+	CHECK_MSTATUS(nAttr.setMax(1.0));
+	CHECK_MSTATUS(addAttribute(blendWeight));
+
+	attributeAffects(blendMesh, outputGeom);
+	attributeAffects(blendWeight, outputGeom);
+
+	MGlobal::executeCommand("makePaintable -attrType multiFloat -sm deformer SingleBlendMesh weights");
+
+	return MStatus::kSuccess;
+}
+
+MStatus SingleBlendMeshDeformer::deform(MDataBlock & block, MItGeometry & iterator, const MMatrix & matrix, unsigned int multiIndex)
+{
+	MPlug blendMeshPlug{ thisMObject(), blendMesh };
+	if (!blendMeshPlug.isConnected()) {
+		MGlobal::displayWarning(this->name() + ": blendMesh not connected. Please connect a mesh.");
+		return MStatus::kInvalidParameter;
+	}
+
+	float envelopeValue{ block.inputValue(envelope).asFloat() };
+	MObject blendMeshValue{ block.inputValue(blendMesh).asMesh() };
+	double blendWeightValue{ block.inputValue(blendWeight).asDouble() };
+
+	MFnMesh blendMeshFn{ blendMeshValue };
+
+	for (iterator.reset(); !iterator.isDone(); iterator.next()) {
+		MPoint currentPosition{ iterator.position()};
+
+		MPoint targetPosition{};
+		CHECK_MSTATUS_AND_RETURN_IT( blendMeshFn.getPoint(iterator.index(), targetPosition) );
+
+		float weight{ weightValue(block, multiIndex, iterator.index()) };
+		MVector delta{ (targetPosition - currentPosition) * blendWeightValue * envelopeValue *  weight };
+		MPoint newPosition{ delta + currentPosition };
+
+		iterator.setPosition(newPosition);
+	}
+	
+	return MStatus::kSuccess;
+}
